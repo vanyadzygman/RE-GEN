e859cb8e6b3e48b22294d654d723b61adbd8ff81
Ivan <vanyadzygman@gmail.com>
2026-04-30 23:36:41 +0300

    small changes

--- src/components/form/section_template_design.ts
+++ src/components/form/section_template_design.ts
@@ -1,19 +1,19 @@
 export function getTemplateDesignHTML() {
     return `
         <div class="section-title">ШАБЛОН ОФОРМЛЕННЯ</div>
-                <div class="options">
-                    <button class="button">
-                        <span class="icon">📄</span>
-                        CLASSIC
-                    </button>
-                    <button class="button">
-                        <span class="icon">🎨</span>
-                        MODERN
-                    </button>
-                    <button class="button">
-                        <span class="icon">✨</span>
-                        CREATIVE
-                    </button>
-                </div>
+            <div class="options">
+                <button class="button">
+                    <span class="icon">📄</span>
+                    CLASSIC                    
+                </button>
+                <button class="button">
+                    <span class="icon">🎨</span>
+                    MODERN
+                </button>
+                <button class="button">
+                    <span class="icon">✨</span>
+                    CREATIVE
+                </button>
+            </div>
     `;
 }
