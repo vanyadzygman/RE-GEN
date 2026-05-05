5285c838b2b52af639da71fb6ba922d6467cee7a
Ivan <vanyadzygman@gmail.com>
2026-05-06 01:51:26 +0300

    add education and experience sections

--- src/components/form/section_experience.ts
+++ src/components/form/section_experience.ts
@@ -1,5 +1,29 @@
 export function getExperienceHTML() {
     return `
-        <div class="section-title">ДОСВІД РОБОТИ</div>
-    `;
-}
+    <div class="section-title">ДОСВІД РОБОТИ</div>
+    <div class="block">
+        <div class="block-header">
+            <span class="block-title">Місце роботи #1</span>
+            <button class="block-delete">&times</button>
+        </div>
+        <div class="field">
+            <label>Посада</label>
+            <input type="text" placeholder="Frontend Developer">
+        </div>
+        <div class="field">
+            <label>Компанія</label>
+            <input type="text" placeholder="Google">
+        </div>
+        <div class="field-group">
+            <div class="field">
+                <label>Початок</label>
+                <input type="text" placeholder="1999">
+            </div>
+            <div class="field">
+                <label>Кінець</label>
+                <input type="text" placeholder="2026">
+            </div>
+        </div>
+    </div>
+  `;    
+} 
